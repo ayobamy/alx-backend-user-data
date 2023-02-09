1b7c840e591162c35bb55e5f7ce24ecac23691a8
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-09 00:34:03 +0000

    [fix]: fixed bug

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,7 +21,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def error_unauthorized(error) -> str:
+def unauthorized(error) -> str:
     """
     Unauthorized handler
     """
