1917488a983f1f119e11d8bcfbfb99e19c62df95
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-09 01:29:01 +0000

    [feat]: basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Auth
+"""
+from flask import request
+
+
+class Auth:
+    """
+    a class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        a method that returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        returns the authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns current user information
+        """
+        return None
