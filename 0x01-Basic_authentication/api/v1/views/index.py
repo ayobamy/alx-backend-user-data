c92efcd29f3fa385e4f53368311bde0800df1137
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-09 00:50:45 +0000

    [update]: testting the handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,7 +35,7 @@ def unauthorized() -> str:
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def forbidden() -> str:
     """
     raise a 403 (forbidden) error by using abort
     """
