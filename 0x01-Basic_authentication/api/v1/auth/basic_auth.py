db1e24fa4365c5b0407b65d1cc40f419c89275e2
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-10 23:19:56 +0000

    [update]: fix bug

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,5 +37,5 @@ class BasicAuth(Auth):
             decoded_value = base64.b64decode(base64_authorization_header)
             decoded_value = decoded_value.decode('utf-8')
             return decoded_value
-        except Exception as error:
+        except Exception as e:
             return None
