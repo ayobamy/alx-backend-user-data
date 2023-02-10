88a0e0143bf03d54a1dee70dc5b22ba4124eecc6
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-10 21:45:42 +0000

    [update]: require authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,18 @@ class Auth:
         """
         a method that returns False
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exc_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exc_path[-1] == "*":
+                    pattern = "{}.*".format(exc_path[0:-1])
+                elif exc_path[-1] == "/":
+                    pattern = "{}/*".format(exc_path[0:-1])
+                else:
+                    pattern = "{}/*".format(exc_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
