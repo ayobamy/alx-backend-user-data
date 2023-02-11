13ef0a32bdde5fab77ef20b1a71a91683dfd74f8
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-11 20:55:34 +0000

    [fix]: fixed bug

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -74,7 +74,7 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         list_users = User.search({'email': user_email})
-        if len(list_users) == 0:
+        if len(list_users) <= 0:
             return None
         user = list_users[0]
         if not user.is_valid_password(user_pwd):
