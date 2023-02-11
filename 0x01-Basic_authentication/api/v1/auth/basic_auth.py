235fb172821670c70d25f77ae66cfed3195baad9
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-11 20:21:08 +0000

    [update]: user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,22 @@ class BasicAuth(Auth):
             return (None, None)
         email, passwrd = decoded_base64_authorization_header.split(":")
         return email, passwrd
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str) \
+            -> TypeVar('User'):
+        """
+        returns the User instance based on
+        his email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        list_users = User.search(user_email)
+        if len(list_users) == 0:
+            return None
+        user = list_users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
